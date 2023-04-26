135d3f21fa2261065b644fce7d863d0111f112d2
Hari Bhandari <2012bhandari.ha@gmail.com>
2023-04-26 15:53:21 +0100

    initial

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ data_transforms = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+
 # loading dataset directly from torchvision as suggested in our paper and we split the dataset into train, val, and test
 train_dataset = torchvision.datasets.Flowers102(root='./data', split='train', transform=data_transforms, download=True)
 val_dataset = torchvision.datasets.Flowers102(root='./data', split='val', transform=data_transforms, download=True)
