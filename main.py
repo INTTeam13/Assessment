b4979679ed55624e3ee64b627b2c58bc32ac3333
Hari Bhandari <61126375+hari-bhandari@users.noreply.github.com>
2023-05-02 18:36:00 +0100

    working model :rocket: 47%

--- main.py
+++ main.py
@@ -1,23 +1,182 @@
+from multiprocessing import freeze_support
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
-# notethat flowers102 dataset require scipy
-# Define the data transformations for the dataset, so these will be applied to each image that are in the dataset
-data_transforms = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224), # we're resizing the image to 256x256 and then cropping the center to 224x224
+import torchvision.models as models
+import torch.nn as nn
+import torch.optim as optim
+class CustomCNN(nn.Module):
+    def __init__(self, num_classes=102):
+        super(CustomCNN, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(512 * 14 * 14, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+data_transforms_train = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomRotation(45),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
     transforms.ToTensor(),
-    # found this on the internet, but to be changed later
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+data_transforms_val = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+data_transforms_test = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 
 # loading dataset directly from torchvision as suggested in our paper and we split the dataset into train, val, and test
-train_dataset = torchvision.datasets.Flowers102(root='./data', split='train', transform=data_transforms, download=True)
-val_dataset = torchvision.datasets.Flowers102(root='./data', split='val', transform=data_transforms, download=True)
-test_dataset = torchvision.datasets.Flowers102(root='./data', split='test', transform=data_transforms, download=True)
+train_dataset = torchvision.datasets.Flowers102(root='./data', split='train', transform=data_transforms_train,
+                                                download=True)
+val_dataset = torchvision.datasets.Flowers102(root='./data', split='val', transform=data_transforms_val, download=True)
+test_dataset = torchvision.datasets.Flowers102(root='./data', split='test', transform=data_transforms_test,
+                                               download=True)
 
 # Create data loaders to load the data in batches
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=2)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=2)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=False)  # removed num_workers=2
+
+
+def set_device():
+    if torch.cuda.is_available():
+        dev = "cuda:0"
+    else:
+        dev = "cpu"
+    return torch.device(dev)
+
+
+def evaluate_model_on_test_set(model, test_loader):
+    model.eval()
+    predicted_correctly_on_epoch = 0
+    total = 0
+    device = set_device()
+
+    with torch.no_grad():  # Speeds up process, does not allow back-prop
+        for data in test_loader:
+            # Specifies batch size incase last batch does not end on even multiple of batch size
+            # (e.g. 29 images in last batch not 32)
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            total += labels.size(0)
+
+            outputs = model(images)  # Models classification of the images
+
+            _, predicted = torch.max(outputs.data, 1)  # 1 specifies dimension it is reduced to
+
+            predicted_correctly_on_epoch += (labels == predicted).sum().item()
+
+    epoch_accuracy = (predicted_correctly_on_epoch / total) * 100  # Get accuracy as percentage
+
+    print("Test dataset - Classified %d out of %d images correctly (%.3f%%)" %
+          (predicted_correctly_on_epoch, total, epoch_accuracy))
+
+def train_network(model, train_loader, test_loader, loss_function, optimizer, n_epochs):
+    device = set_device()
+
+    for epoch in range(n_epochs):
+        print("Epoch number %d " % (epoch+1))
+        model.train()
+        running_loss = 0.0
+        running_correct = 0.0
+        total = 0
+
+        for data in train_loader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            total += labels.size(0)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)  # Models classification of the images
+
+            _, predicted = torch.max(outputs.data, 1)  # 1 specifies dimension it is reduced to
+
+            loss = loss_function(outputs, labels)
+
+            loss.backward()  # Back propagate through network
+
+            optimizer.step()  # Update weights
+
+            running_loss += loss.item()
+            running_correct += (labels == predicted).sum().item()
+
+        epoch_loss = running_loss / len(train_loader)
+        epoch_accuracy = (running_correct / total) * 100  # Get accuracy as percentage
+
+        print("Training dataset - Classified %d out of %d images correctly (%.3f%%). Epoch loss: %.3f" %
+              (running_correct, total, epoch_accuracy, epoch_loss))
+
+    evaluate_model_on_test_set(model, test_loader)
+
+    print("Finished")
+    return model
+
+
+
+# Instantiate the custom model
+custom_model = CustomCNN()
+device = set_device()
+custom_model = custom_model.to(device)
+loss_function = nn.CrossEntropyLoss()
+
+optimizer = optim.SGD(custom_model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.003)
+
+train_network(custom_model, train_loader, test_loader, loss_function, optimizer, 400)
