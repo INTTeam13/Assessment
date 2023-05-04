929f2c437aafe460f92c7f95db67399213350f23
Hari Bhandari <2012bhandari.ha@gmail.com>
2023-05-04 08:29:46 +0100

    changed model

--- test_model_on_test.py
+++ test_model_on_test.py
@@ -75,7 +75,7 @@ class CustomResNet(nn.Module):
 
 
 data_transforms_test_val = transforms.Compose([
-    transforms.Resize(256),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
